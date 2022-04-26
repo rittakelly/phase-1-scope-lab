48cf4bf55ae680432e315c9612ce615097943c41
Ritta <brittanykelly33@gmail.com>
2022-04-26 03:23:31 -0400

    commit tree 06d57f37cc9327964354f145ec91ff4bf461e7c9

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = 'BOB';
+    return customerName;
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = 'Karen'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Carol'
+}
